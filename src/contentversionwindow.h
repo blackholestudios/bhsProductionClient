01bc9e712e949f32c57bb0311eb9204df7227433
Daniel Sirota <dsirota@purdue.edu>
2019-11-05 18:05:39 -0500

    commited

--- src/contentversionwindow.h
+++ src/contentversionwindow.h
@@ -22,15 +22,15 @@ public:
     serverItems content;
 
 
-    QLabel *fullNameLabel=new QLabel("full name not recovered");
-    QLabel *thumbnailLabel=new QLabel("Image not found");
-    QLabel *statusLabel =new QLabel("Status not found");
-    QLabel *deadlineLabel =new QLabel("Deadline not found");
-    QLabel *deadlineNameLabel =new QLabel("Deadline");
-    QLabel *typeLabel =new QLabel("Type not fond");
-    QLabel *typeNameLabel =new QLabel("Type");
-    QLabel *pipelineStageLabel =new QLabel("Pipeline stage not found");
-    QLabel *pipelineStageNameLabel =new QLabel("Pipeline");
+    QLabel *fullNameLabel = new QLabel("full name not recovered");
+    QLabel *thumbnailLabel = new QLabel("Image not found");
+    QLabel *statusLabel = new QLabel("Status not found");
+    QLabel *deadlineLabel = new QLabel("Deadline not found");
+    QLabel *deadlineNameLabel = new QLabel("Deadline");
+    QLabel *typeLabel = new QLabel("Type not fond");
+    QLabel *typeNameLabel = new QLabel("Type");
+    QLabel *pipelineStageLabel = new QLabel("Pipeline stage not found");
+    QLabel *pipelineStageNameLabel = new QLabel("Pipeline");
     QImageReader *pic;
     QVBoxLayout *mainLayout = new QVBoxLayout();
     QHBoxLayout *hbox = new QHBoxLayout();
--- src/unass.h
+++ src/unass.h
@@ -17,15 +17,15 @@ public:
     serverItems content;
 
 private:
-    QLabel *fullNameLabel=new QLabel("full name not recovered");
-    QLabel *thumbnailLabel=new QLabel("Image not found");
-    QLabel *statusLabel =new QLabel("Status not found");
-    QLabel *deadlineLabel =new QLabel("Deadline not found");
-    QLabel *deadlineNameLabel =new QLabel("Deadline");
-    QLabel *typeLabel =new QLabel("Type not fond");
-    QLabel *typeNameLabel =new QLabel("Type");
-    QLabel *pipelineStageLabel =new QLabel("Pipeline stage not found");
-    QLabel *pipelineStageNameLabel =new QLabel("Pipeline");
+    QLabel *fullNameLabel = new QLabel("full name not recovered");
+    QLabel *thumbnailLabel = new QLabel("Image not found");
+    QLabel *statusLabel = new QLabel("Status not found");
+    QLabel *deadlineLabel = new QLabel("Deadline not found");
+    QLabel *deadlineNameLabel = new QLabel("Deadline");
+    QLabel *typeLabel = new QLabel("Type not fond");
+    QLabel *typeNameLabel = new QLabel("Type");
+    QLabel *pipelineStageLabel = new QLabel("Pipeline stage not found");
+    QLabel *pipelineStageNameLabel = new QLabel("Pipeline");
     QPushButton *assToME;
     QImageReader *pic;
 
